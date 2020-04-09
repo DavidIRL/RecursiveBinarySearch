147d4252870765bb1e9fa6b791f073bd40bed063
David White Jr <61447823+DavidIRL@users.noreply.github.com>
2020-04-08 22:14:24 -0500

    Add additional test cases to binary_search.py

--- binary_search.py
+++ binary_search.py
@@ -1,8 +1,9 @@
 def binary_search(arr, target):
+    arr.sort()
     return b_search_func(arr, 0, len(arr) - 1, target)
 
 def b_search_func(arr, start_index, end_index, target):
-    # test for appropriate index order
+    # if start_index is less than end_index target was not found
     if start_index > end_index:
         return -1
     
@@ -18,6 +19,12 @@ def b_search_func(arr, start_index, end_index, target):
         
 if __name__ == '__main__':
     arr = ['apple', 'peach', 'blueberry', 'cherry', 'lemon', 'key lime', 'rhubarb', 'chocolate creme']
-    binary_search(arr, 'cherry')
-    binary_search(arr, 'rhubarb')
-    binary_search(arr, 'meat')
+    print(binary_search(arr, 'cherry'))
+    print(binary_search(arr, 'rhubarb'))
+    print(binary_search(arr, 'meat'))
+    print('\n')
+    arr2 = [2, 3, 9, 7, 4, 6, 8, 1, 25, 81, 96, 35, 5, 0, 774, 11]
+    print(binary_search(arr2, 1))
+    print(binary_search(arr2, 6))
+    print(binary_search(arr2, 20))
+    print(binary_search(arr2, 774))
